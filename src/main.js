04c3dc91ecdfa2e0ceec74351bfe275439cdd39b
Akinpelumi Taiwo <68600360+Twain97@users.noreply.github.com>
2023-08-29 16:05:35 -1100

    installed primeVue and epic-spinner

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import Button from "primevue/button"
 import App from './App.vue'
 import router from './router'
 
@@ -13,5 +16,7 @@ const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(PrimeVue)
+app.component('Button', Button)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,14 +3,25 @@
     <h1>Home</h1><font-awesome-icon icon="fa-solid fa-user-secret" />
 
     <b>count = {{ count }}</b>
-
-    <button @click="counter()" class="ml-14 border-4 rounded-md border-blue-400">Counter</button>
+    <Button label="Check" icon="pi pi-check" class=" ml-9" @click="counter()"/>
+  
+    <div>
+      <orbit-spinner
+      :animation-duration="1200"
+      :size="55"
+      color="#ff1d5e"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import { OrbitSpinner } from 'epic-spinners'
 export default {
+  components: {
+    OrbitSpinner
+  },
   computed: {
     ...mapState({
       count: "count"
